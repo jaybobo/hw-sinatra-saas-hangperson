23a6aab1ad6c1bbe2ebd3661a5ba8318835b574e
bwilkins <bwilkins@covermymeds.com>
2015-10-31 13:09:25 -0400

    completed part 1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,10 @@
 class HangpersonGame
+  attr_reader :word, :guesses, :wrong_guesses
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -18,5 +13,35 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+
+  def letter?(letter)
+    letter =~ /[A-Za-z]/
+  end
+
+  def guess(letter)
+    raise ArgumentError if !letter?(letter)
+    letter.downcase!
+    repeat = @guesses.include?(letter)|| @wrong_guesses.include?(letter)
+    if !repeat
+      valid = @word.include? letter
+      @guesses << letter if valid
+      @wrong_guesses << letter unless valid
+      true
+    else 
+      false
+    end
+  end
+
+  def word_with_guesses
+    show_word = @word.chars.collect{ |char| @guesses.include?(char) ? char : '-'}.join('')
+  end
+
+  def check_win_or_lose
+    return :win if @guesses.length == @word.length
+    return :lose if @wrong_guesses.length == 7
+    :play
+  end
+
+
 
 end
